3f7a621e53060c3702c059ebe01a233568473ab2
ToanNguyenThai <19522366@gm.uit.edu.vn>
2022-05-27 15:46:59 +0700

    update

--- carouseConfig.js
+++ carouseConfig.js
@@ -2,7 +2,9 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-
+    autoplay: {
+        delay: 3000
+    },
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
